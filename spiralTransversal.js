0fdcb1a8988bdd475ec89696da28c23c40e8db77
Michelle <mtham101@gmail.com>
2016-05-19 20:18:18 -0700

    spiralTraversal solved

--- spiralTransversal.js
+++ spiralTransversal.js
@@ -2,13 +2,36 @@
 // Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),
 // and prints out every value found, but in a spiral from the upper left in to the center.
 
-// INPUT: [ [ 1, 2, 3, 4, 5 ], [ 6, 7, 8, 9, 10 ], [ 11, 12, 13, 14, 15 ], [ 16, 17, 18, 19, 20 ], [ 21, 22, 23, 24, 25 ] ]
+// INPUT:
+// [
+// [ 1, 2, 3, 4, 5 ],
+// [ 6, 7, 8, 9, 10 ],
+// [ 11, 12, 13, 14, 15 ],
+// [ 16, 17, 18, 19, 20 ],
+// [ 21, 22, 23, 24, 25 ]
+// ]
 // OUTPUT: [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 ]
 
-// INPUT: [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 10, 11, 12 ], [ 13, 14, 15 ], [ 16, 17, 18 ], [ 19, 20, 21 ], [ 22, 23, 24 ] ]
+// INPUT:
+// [
+// [ 1, 2, 3 ],
+// [ 4, 5, 6 ],
+// [ 7, 8, 9 ],
+// [ 10, 11, 12 ],
+// [ 13, 14, 15 ],
+// [ 16, 17, 18 ],
+// [ 19, 20, 21 ],
+// [ 22, 23, 24 ]
+// ]
 // OUTPUT: [ 1, 2, 3, 6, 9, 12, 15, 18, 21, 24, 23, 22, 19, 16, 13, 10, 7, 4, 5, 8, 11, 14, 17, 20 ]
 
-// INPUT: [ [ 1 ], [ 2 ], [ 3 ], [ 4 ] ]
+// INPUT:
+// [
+// [ 1 ],
+// [ 2 ],
+// [ 3 ],
+// [ 4 ]
+// ]
 // OUTPUT:   [ 1, 2, 3, 4 ]
 
 function spiralTraversal (matrix) {
@@ -17,6 +40,53 @@ function spiralTraversal (matrix) {
   var bottom = matrix.length
   var left = 0
   var right = matrix[0].length
+
+  function goRight () {
+    if(left === right) {
+      return result
+    }
+    for (var i = left; i < right; i++) {
+      result.push(matrix[top][i])
+    }
+    top++
+    goDown()
+  }
+
+  function goDown () {
+    if(top === bottom) {
+      return result
+    }
+    for (var j = top; j < bottom; j++) {
+      result.push(matrix[j][right-1])
+    }
+    right--
+    goLeft()
+  }
+
+  function goLeft () {
+    if(left === right) {
+      return result
+    }
+    for (var k = right; k > left; k--) {
+      result.push(matrix[bottom-1][k-1])
+    }
+    bottom--
+    goUp()
+  }
+
+  function goUp () {
+    if (top === bottom) {
+      return result
+    }
+    for (var l = bottom-1; l >= top; l--) {
+      result.push(matrix[l][left])
+    }
+    left++
+    goRight()
+  }
+
+  goRight()
+  return result
   // call goRight function
     // check if left === right
       // return result
