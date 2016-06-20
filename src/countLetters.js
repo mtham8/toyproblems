2ee330054f82b2ef8d83b6cbf30799b7ca6a1c3d
Michelle <mtham101@gmail.com>
2016-06-20 13:08:38 -0700

    [Refactor] Added pseudocode

--- src/countLetters.js
+++ src/countLetters.js
@@ -1,5 +1,15 @@
 // Given a string 'AABBBCCCDDAAA', return the output = '2A3B3C2D3A'
 
+// plain English:
+// 1. create a counter variable
+// 2. create a results variable
+// 3. iterate over the string
+// 3a. if the current letter is the same as the next letter
+// 3a. increment the counter
+// 3b. else, add the count of the current letter and the letter to the results string
+// 3b. reset the counter
+// 4. return the results
+
 const countLetters = string => {
   let counter = 1;
   let results = '';
