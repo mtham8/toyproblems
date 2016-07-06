114066ba56726808407b7e123552e136ebd898ce
Michelle <mtham101@gmail.com>
2016-07-05 21:11:10 -0700

    clean up console logs for longCountString

--- src/longCountString.js
+++ src/longCountString.js
@@ -4,10 +4,8 @@
 function longCountString (string) {
   var longest = 0;
   var splitString = string.split(' ');
-  console.log('split string', splitString);
   splitString.forEach(function (el) {
     if(longest < el.length) {
-      console.log('element length', el.length)
       longest = el.length;
     }
   })
