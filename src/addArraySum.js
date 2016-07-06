5023711b9fd5757ae0de7127b55ba0c4a136c8e5
Michelle <mtham101@gmail.com>
2016-07-05 20:30:34 -0700

    remove console logs

--- src/addArraySum.js
+++ src/addArraySum.js
@@ -9,15 +9,12 @@ function addArray (array) {
   var output = [];
   for (var i = 1; i < array.length; i++) {
     left[i] = left[i-1] + array[i-1];
-    console.log('this is left plus array', left[i], "=", left[i-1], "+", array[i-1])
   }
   for (var j = array.length-2; j >= 0; j--) {
     right[j] = right[j+1] + array[j+1];
-    console.log('this is right plus array', right[j], "=", right[j+1], "+", array[j+1])
   }
   for (var k = 0; k < array.length; k++) {
     output[k] = left[k] + right[k];
-    console.log('this is output', output[k], "=", left[k], "+", right[k])
   }
   return output;
 }
