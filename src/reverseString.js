4856c966b0b5686822909d4ff76a8a3b4212423e
Michelle <mtham101@gmail.com>
2016-09-28 20:30:33 -0700

    reverseString.js solved

---
+++ src/reverseString.js
@@ -0,0 +1,8 @@
+/*
+4. Print Backwards. Print a string backwards
+    input: printBackwards('abcdefg')
+    output: 'gfedcba'
+*/
+
+const printBackwards = string => string.split('').reverse().join('')
+
