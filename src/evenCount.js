fe8b207f6d2b2d75ac74ea3db9e519b4f769075c
Michelle Tham <mtham101@gmail.com>
2016-09-29 15:34:52 -0700

    fix evenCount

--- src/evenCount.js
+++ src/evenCount.js
@@ -5,4 +5,10 @@
   output: 2, 2, 4, 4
 */
 
-const evenCount = array => array.filter(item => item % 2 === 0)
+const evenCount = (array, obj = {}) => array.map(a => {
+  obj[a] = obj[a]+1 || 1
+  return a
+  }).reduce((a, b) => {
+    if(obj[b] % 2 == 0) a.push(b)
+    return a
+  },[])
