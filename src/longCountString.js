e42777956562b57da4db997b7ccf109d4818b028
Michelle Tham <mtham101@gmail.com>
2016-09-29 16:04:08 -0700

    refactor longCountString.js

--- src/longCountString.js
+++ src/longCountString.js
@@ -1,15 +1,16 @@
 // input: "Always be committing to this repo"
 // output: 10
 
-function longCountString (string) {
-  var longest = 0;
-  var splitString = string.split(' ');
-  splitString.forEach(function (el) {
-    if(longest < el.length) {
-      longest = el.length;
-    }
-  })
-  return longest;
-}
+// function longCountString (string) {
+//   var longest = 0;
+//   var splitString = string.split(' ');
+//   splitString.forEach(function (el) {
+//     if(longest < el.length) {
+//       longest = el.length;
+//     }
+//   })
+//   return longest;
+// }
 
-longCountString("Always be committing to this repo");
+// Refactored
+const longCountString = string => string.split(' ').reduce((a, b) => a > b.length ? a : b.length, 0)
