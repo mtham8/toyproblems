edd7eb0d10b14da05b6001a3af22353552cfd718
Michelle Tham <mtham101@gmail.com>
2016-10-05 12:20:42 -0700

    deeply flatten array using reduce

--- src/flattenArray.js
+++ src/flattenArray.js
@@ -1,3 +1,7 @@
 // flatten deeply nested arrays recursively
 
+// using map
 const flatten = arr => Array.isArray(arr) ? [].concat(...arr.map(flatten)) : arr;
+
+// using reduce
+const flatten = arr => arr.reduce((prev, curr) => Array.isArray(curr) ? prev.concat(flatten(curr)) : prev.concat(curr), []);
